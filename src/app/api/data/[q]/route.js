74e3bdce135aee52c39969db8436aa482600e58f
Shank0071 <sankarpv.pmc@gmail.com>
2023-07-17 21:53:18 +0530

    first commit

---
+++ src/app/api/data/[q]/route.js
@@ -0,0 +1,25 @@
+import { load } from "cheerio";
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, {params}) => {
+  console.log("q", params)
+  const { q } = params
+  async function getData() {
+    const { data } = await axios.get(
+      `https://www.bing.com/search?q=${q}`
+    );
+    const $ = load(data);
+    const links = [];
+    $("a").each((index, element) => {
+      const href = $(element).attr("href");
+      if (href && href.startsWith("https")) {
+        links.push(href);
+      }
+    });
+
+    return links;
+  }
+  const data = await getData();
+  return NextResponse.json({data}, { status: 201 });
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,73 @@
-import Image from 'next/image'
+"use client";
+
+import axios from "axios";
+import { useState, CSSProperties } from "react";
+import { ClipLoader } from "react-spinners";
+
+async function getData(q) {
+  const data = await axios.get(`http://localhost:3000/api/data/${q}`);
+  return data.data.data;
+}
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [arrLinks, setArrLinks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [query, setQuery] = useState("");
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const links = await getData(query);
+      console.log(links);
+      setArrLinks(links);
+      console.log(arrLinks);
+      setLoading(false);
+    } catch (err) {
+      setError(true);
+      console.log(err);
+      setLoading(false);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+    setQuery("");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="max-w-[650px] mx-auto mt-[50px] flex flex-col gap-5 items-center">
+      <h1 className="font-bold text-4xl bg-gradient-to-r from-purple-700 via-purple-900 to-slate-500 bg-clip-text text-transparent">Link Scraper</h1>
+      <form onSubmit={handleSubmit} className="flex gap-4">
+        <input
+          className="border border-blue-500 p-2"
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          placeholder="Enter query..."
+          required
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <button className="bg-green-500 text-white p-2 rounded-md">Get</button>
+      </form>
+      {loading && <ClipLoader />}
+      {error && <h3>Something went wrong</h3>}
+      {(arrLinks.length !== 0) && (
+        <div className="flex flex-col p-4 rounded-md bg-slate-200">
+          {arrLinks &&
+            arrLinks.map((link, index) => (
+              <a
+                className="text-blue-900 hover:text-blue-700"
+                key={index}
+                href={link}
+              >
+                {link}
+              </a>
+            ))}
+        </div>
+      )}
+      {(arrLinks.length === 0) && (<h3 className="text-slate-500">Enter Query. You will see the results here...</h3>)}
     </main>
-  )
+  );
 }
